e31933dada2b220c8e17d8af0abfc8c384483fad
carl <carlchen8617@gmail.com>
2020-02-16 21:33:41 +1100

    add spark scripts

---
+++ spark/readMysqlSpark.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import pyspark 
+import os
+from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import col
+ 
+
+os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
+
+
+spark = pyspark.sql.SparkSession.builder \
+       .master("local") \
+       .appName("Word Count") \
+       .config("spark.some.config.option", "some-value") \
+       .getOrCreate()
+
+query="(select * from test.pet) t1_alias"
+
+jj=spark.read.format('jdbc')\
+            .option("url", "jdbc:mysql://localhost/test")\
+            .option("driver", "com.mysql.jdbc.Driver")\
+            .option("dbtable",  query)\
+            .option("user", "carl")\
+            .option("password", "Fangpi!234")\
+            .option("serverTimezone","UTC")\
+            .load()
+
+jj.show()
+print(jj)
+
+
---
+++ spark/writeMysqlSpark.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import pyspark 
+import os
+from pyspark.sql.types import TimestampType
+from pyspark.sql.functions import col
+ 
+
+os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
+
+
+spark = pyspark.sql.SparkSession.builder \
+       .master("local") \
+       .appName("Word Count") \
+       .config("spark.some.config.option", "some-value") \
+       .getOrCreate()
+
+
+d=[{'name':'Puffball','owner':'Diane','species':'hamster','sex':'f','birth':'1999-03-30','death':'0001-01-01'}]
+ki=spark.createDataFrame(d)       
+col("death").cast(TimestampType())
+
+jj=ki.write.jdbc("jdbc:mysql://localhost/test",\
+                 "test.pet",\
+                 "append",\
+                 {"driver":"com.mysql.jdbc.Driver",\
+                  "user":"carl",\
+                  "password":"Fangpi!234",\
+                  "serverTimezone":"UTC"}\
+                 )
+print(jj)
+
+
